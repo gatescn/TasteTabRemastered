6581745610e71bd269fc05459670bff39a08a0e1
Corey Gates <gatescn@vcu.edu>
2018-12-02 13:16:54 -0500

    first commit of base project

---
+++ app/src/main/java/com/example/coreygates/tastetab/Index.kt
@@ -0,0 +1,12 @@
+package com.example.coreygates.tastetab
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Index : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_index)
+    }
+}
